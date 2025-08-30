21c9063c7e408f24cd27af71e7c62e7bf37d0e72
karthibtech523 <karthibtech523@gmail.com>
2025-08-30 05:50:48 +0000

    first page

---
+++ lib/signup_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Create Account")),
+      body: const Center(
+        child: Text("Signup Page (to be implemented)"),
+      ),
+    );
+  }
+}
